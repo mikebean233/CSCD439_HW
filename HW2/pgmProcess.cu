12c8c430dccdaa202b72f679014cfa3cbd71fe62
Michael Peterson <mikebean233@yahoo.com>
2016-04-24 21:29:31 -0700

    no message

--- HW2/pgmProcess.cu
+++ HW2/pgmProcess.cu
@@ -23,35 +23,47 @@ int  pgmDrawEdge(int *pixels, int numRows, int numCols, int edgeWidth, char **he
     int* dPixels;
     int blockSize = 64;
     int gridSize = ceil(((double)numRows * (double)numCols) / (double) blockSize);
-    printf("gridsize: %d\n", gridSize);
     int arraySizeInBytes = sizeof(int) * numRows * numCols;
 
     // allocate device memory for the array
-    printf("cudaMalloc()\n");
     cudaMalloc(&dPixels, arraySizeInBytes);
 
-    // zero the memory in cuda
-    //cudaMemset(d_array, 0, arraySizeInBytes);
-
     // copy the cpu memory to the gpu
-    printf("cudaMemcpy()\n");
     cudaMemcpy(dPixels, pixels, arraySizeInBytes, cudaMemcpyHostToDevice);
 
     // run the kernel
-    printf("gpuDrawEdge()\n");
-    gpuDrawEdge<<<gridSize, blockSize>>>(dPixels, numRows, numCols, edgeWidth);
+    gpuDrawEdge<<<gridSize, blockSize>>>(dPixels, numRows, numCols, edgeWidth, numRows * numCols);
 
     // copy the results back to the host array
-    printf("cudaMemcpy()\n");
     cudaMemcpy(pixels, dPixels, arraySizeInBytes, cudaMemcpyDeviceToHost);
 
     // release the device array
-    printf("cudaFree()\n");
     cudaFree(dPixels);
     return 0;
 }
 
 int pgmDrawCircle(int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius, char **header) {
+
+    int* dPixels;
+    int blockSize = 64;
+    int gridSize = ceil(((double)numRows * (double)numCols) / (double) blockSize);
+    int arraySizeInBytes = sizeof(int) * numRows * numCols;
+
+    // allocate device memory for the array
+    cudaMalloc(&dPixels, arraySizeInBytes);
+
+    // copy the cpu memory to the gpu
+    cudaMemcpy(dPixels, pixels, arraySizeInBytes, cudaMemcpyHostToDevice);
+
+    // run the kernel
+    gpuDrawCircle<<<gridSize, blockSize>>>(dPixels, numRows, numCols, centerRow, centerCol, radius, numRows * numCols);
+
+    // copy the results back to the host array
+    cudaMemcpy(pixels, dPixels, arraySizeInBytes, cudaMemcpyDeviceToHost);
+
+    // release the device array
+    cudaFree(dPixels);
+
     return 0;
 }
 
@@ -59,7 +71,7 @@ int pgmDrawLine(int *pixels, int numRows, int numCols, char **header, int p1row,
     return 0;
 }
 
-__global__ void gpuDrawEdge(int *pixels, int numRows, int numCols, int edgeWidth) {
+__global__ void gpuDrawEdge(int *pixels, int numRows, int numCols, int edgeWidth, int n) {
     int threadId = blockIdx.x * blockDim.x + threadIdx.x;
     int thisRow  = threadId / numCols;
     int thisCol  = threadId % numCols;
@@ -72,15 +84,19 @@ __global__ void gpuDrawEdge(int *pixels, int numRows, int numCols, int edgeWidth
     }
 }
 
-__global__ void gpuDrawCircle(int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius, char **header) {
+__global__ void gpuDrawCircle(int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius, int n) {
     int threadId = blockIdx.x * blockDim.x + threadIdx.x;
     int thisRow  = threadId / numCols;
     int thisCol  = threadId % numCols;
 
+    int p1[] = {thisRow, thisCol};
+    int p2[] = {centerRow, centerCol};
 
+    if(distance(p1, p2) <= radius)
+        pixels[threadId] = 0;
 }
 
-__global__ void gpuDrawLine(int *pixels, int numRows, int numCols, char **header, int p1row, int p1col, int p2row, int p2col) {
+__global__ void gpuDrawLine(int *pixels, int numRows, int numCols, char **header, int p1row, int p1col, int p2row, int p2col, int n) {
 
 }
 
--- HW2/pgmProcess.h
+++ HW2/pgmProcess.h
@@ -11,6 +11,6 @@
 __device__ float distance( int p1[], int p2[] );
 
 
-__global__ void gpuDrawEdge(int *pixels, int numRows, int numCols, int edgeWidth);
-__global__ void gpuDrawCircle(int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius);
-__global__ void gpuDrawLine(int *pixels, int numRows, int numCols, char **header, int p1row, int p1col, int p2row, int p2col);
+__global__ void gpuDrawEdge(int *pixels, int numRows, int numCols, int edgeWidth, int n);
+__global__ void gpuDrawCircle(int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius, int n);
+__global__ void gpuDrawLine(int *pixels, int numRows, int numCols, char **header, int p1row, int p1col, int p2row, int p2col, int n);
