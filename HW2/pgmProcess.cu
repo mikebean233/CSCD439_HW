4d27510c117a03a92fd8dcf954c8be7b11057100
Michael Peterson <mikebean233@yahoo.com>
2016-04-24 21:32:58 -0700

    no message

--- HW2/pgmProcess.cu
+++ HW2/pgmProcess.cu
@@ -13,9 +13,9 @@
  */
 __device__ float distance( int p1[], int p2[] )
 {
-    //int distanceX = p2[1] - p1[1];
-    //int distanceY = p2[0] - p1[0];
-    //return sqrt((float)(distanceX * distanceX) + (float)(distanceY * distanceY));
+    int distanceX = p2[1] - p1[1];
+    int distanceY = p2[0] - p1[0];
+    return sqrt((float)(distanceX * distanceX) + (float)(distanceY * distanceY));
     return 0.0;
 }
 
