c9614a9325350970a15e40e74b65d6fb6158e02a
Michael Peterson <mikebean233@yahoo.com>
2016-05-28 13:16:36 -0700

    no message

--- HW5/main.cu
+++ HW5/main.cu
@@ -11,16 +11,17 @@ void usage(char* programName, FILE* outFileDisc, int exitStatus);
 void errorExit(char* message, int exitStatus);
 void printArray(uint* array, int length);
 
-__global__ void mergSort(uint* in, uint* out, uint n){
+__global__ void mergSort(uint* SrcKey, uint* SrcVal, uint* DstKey, uint* DstVal, uint n){
     int threadId = blockDim.x * blockIdx.x + threadIdx.x;
     if(threadId > n)
         return;
 
-    out[threadId] = threadId;
+    DstKey[threadId] = threadId;
 }
 
 int main(int argc, char** argv){
-    uint *h_inArray, *h_outArray, *d_inArray, *d_outArray;
+    uint *h_SrcKey, *h_SrcVal, *h_DstKey, *h_DstVal;
+    uint *d_SrcKey, *d_SrcKey, *d_DstKey, *d_DstVal;
     uint3 blockDim, gridDim;
     uint n;
 
@@ -41,45 +42,49 @@ int main(int argc, char** argv){
         errorExit((char*)"Error: the number of input elements must be even", 1);
 
     // Allocate memory
-    h_inArray  = (uint*) calloc(sizeof(uint), n);
-    h_outArray = (uint*) calloc(sizeof(uint), n);
-    cudaMalloc((void **)&d_inArray, n * sizeof(uint));
-    cudaMalloc((void **)&d_outArray, n * sizeof(uint));
-
+    h_SrcKey  = (uint*) calloc(sizeof(uint), n);
+    h_SrcVal  = (uint*) calloc(sizeof(uint), n);
+    h_DstKey  = (uint*) calloc(sizeof(uint), n);
+    h_DstVal  = (uint*) calloc(sizeof(uint), n);
+    cudaMalloc((void **)&d_SrcKey, n * sizeof(uint));
+    cudaMalloc((void **)&d_DstKey, n * sizeof(uint));
+    cudaMalloc((void **)&d_SrcKey, n * sizeof(uint));
+    cudaMalloc((void **)&d_DstKey, n * sizeof(uint));
 
     // Copy the host array to device array
-    cudaMemcpy(d_inArray, h_inArray, n * sizeof(uint), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_SrcKey, h_SrcKey, n * sizeof(uint), cudaMemcpyHostToDevice);
 
-
-    if(h_inArray == NULL || h_outArray == NULL)
+    if(h_SrcKey == NULL || h_DstKey == NULL)
         errorExit((char*)"Error: host was unable to allocate array memory", 2);
 
     srand(time(NULL));
 
     uint i;
     for(i = 0; i < n; ++i){
-         h_inArray[i] = rand() % MAX_VALUE;
+        h_SrcKey[i] = rand() % MAX_VALUE;
+        h_SrcVal[i] = i;
     }
 
-    mergSort<<<blockDim, gridDim>>>(d_inArray, d_outArray, n);
+    mergSort<<<blockDim, gridDim>>>(d_SrcKey, d_SrcVal, d_DstKey, d_DstVal, n);
+    cudaMemcpy(h_DstKey, d_DstKey, n * sizeof(uint), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(h_outArray, d_outArray, n * sizeof(uint), cudaMemcpyDeviceToHost);
+    printArray(h_DstKey, n);
 }
 
 void errorExit(char* message, int exitStatus){
-    fprintf(stderr, message);
+    fprintf(stderr, "%s", message);
     exit(exitStatus);
 }
 
 void usage(char* programName, FILE* outFileDisc, int exitStatus){
-    fprintf(outFileDisc, "usage: %s arraySize blockSize");
+    fprintf(outFileDisc, "usage: %s arraySize blockSize\n", programName);
     exit(exitStatus);
 }
 
 void printArray(uint* array, int length){
     int i = 0;
     for(; i < length; ++i){
-        printf("$d\n", array[i]);
+        printf("%d\n", array[i]);
     }
 }
 
